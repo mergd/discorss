47f0a876a3ab685098a379ced6e8dd44248b2b9a
mergd <24722014+mergd@users.noreply.github.com>
2025-11-09 09:37:01 -0800

    .

--- drizzle/migrations/0005_easy_dakota_north.sql
+++ drizzle/migrations/0005_easy_dakota_north.sql
@@ -1,1 +1,35 @@
+DO $$ BEGIN
+ALTER TABLE "feeds" ADD COLUMN "use_archive_links" boolean DEFAULT false NOT NULL;
+EXCEPTION
+WHEN duplicate_column THEN null;
+END $$;
+--> statement-breakpoint
+DO $$ BEGIN
+ALTER TABLE "feeds" ADD COLUMN "last_error_message_at" timestamp;
+EXCEPTION
+WHEN duplicate_column THEN null;
+END $$;
+--> statement-breakpoint
+DO $$ BEGIN
+ALTER TABLE "feeds" ADD COLUMN "ignore_errors" boolean DEFAULT false NOT NULL;
+EXCEPTION
+WHEN duplicate_column THEN null;
+END $$;
+--> statement-breakpoint
+DO $$ BEGIN
+ALTER TABLE "feeds" ADD COLUMN "disable_failure_notifications" boolean DEFAULT false NOT NULL;
+EXCEPTION
+WHEN duplicate_column THEN null;
+END $$;
+--> statement-breakpoint
+DO $$ BEGIN
 ALTER TABLE "feeds" ADD COLUMN "disabled" boolean DEFAULT false NOT NULL;
+EXCEPTION
+WHEN duplicate_column THEN null;
+END $$;
+--> statement-breakpoint
+DO $$ BEGIN
+ALTER TABLE "feeds" ADD COLUMN "language" text;
+EXCEPTION
+WHEN duplicate_column THEN null;
+END $$;
